fee56c137f01915d984d10af822ecf388757d4fd
Thammathip <thammathip.p17@gmail.com>
2024-10-08 04:23:50 +0700

    first commit

---
+++ script.js.js
@@ -0,0 +1,42 @@
+document.querySelectorAll('nav.Bar a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetSection = document.querySelector(this.getAttribute('href'));
+
+        targetSection.scrollIntoView({
+            behavior: 'smooth', 
+            block: 'start' 
+        });
+    });
+});
+
+
+const faders = document.querySelectorAll('.fade-in');
+const sliders = document.querySelectorAll('.slide-up');
+
+const appearOptions = {
+    threshold: 0.2,
+    rootMargin: "0px 0px -50px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function(
+    entries, 
+    appearOnScroll
+) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add('fade-in');
+            appearOnScroll.unobserve(entry.target);
+        }
+    });
+}, appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+});
+
+sliders.forEach(slider => {
+    appearOnScroll.observe(slider);
+});
